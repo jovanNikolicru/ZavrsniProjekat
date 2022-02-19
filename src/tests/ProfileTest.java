c87dfabb4609e257b023fbec81711e62a8e0901c
Jovan Nikolic <jovannikolic@gmail.com>
2022-02-19 15:55:30 +0100

    Capturing screenshot works

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -32,7 +32,7 @@ public class ProfileTest extends BasicTest {
 	
 		js.executeScript("arguments[0].click();",pp.getProfileSaveBtn());
 	
-		Assert.assertTrue(nsp.getMessageOfNotification().equals("Testing screenshot"), "[ERROR] Setup message ditn't show up");
+		Assert.assertTrue(nsp.getMessageOfNotification().equals("Setup Successful"), "[ERROR] Setup message ditn't show up");
 		nsp.waitForMessageToDisappear();
 		ap.logout();
 		Assert.assertTrue(nsp.getMessageOfNotification().equals("Logout Successfull!"),"[ERROR] Logout message didn't show up");
